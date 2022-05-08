277d717c5f80102b8eb2bdc4a807ec04e83d0119
jgc92 <2539918+jgc92@users.noreply.github.com>
2022-05-08 18:42:50 -0500

    Comment TabBarViewController

--- GBM-Challenge/Views/TabBarViewController.swift
+++ GBM-Challenge/Views/TabBarViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// Main TabBar view to showcase charts.
 class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,23 +15,26 @@ class TabBarViewController: UITabBarController {
     }
     
     private func setup() {
+        // Create all chart views.
         let priceChartViewController = UIViewController()
         let percentageChangeChartViewController = UIViewController()
         let volumeChartViewController = UIViewController()
         let changeChartViewController = UIViewController()
         
+        // Set navigation title.
         priceChartViewController.title = "Price vs Time"
         percentageChangeChartViewController.title = "Percentage Change vs Time"
         volumeChartViewController.title = "Volume vs Time"
         changeChartViewController.title = "Change vs Time"
                 
+        // Create navigations for charts.
         let priceTab = UINavigationController(rootViewController: priceChartViewController)
         let percentageChangeTab = UINavigationController(rootViewController: percentageChangeChartViewController)
         let volumeTab = UINavigationController(rootViewController: volumeChartViewController)
         let changeTab = UINavigationController(rootViewController: changeChartViewController)
         
-        priceTab.title = "Price"
-        priceTab.navigationBar.isTranslucent = false
+        priceTab.title = "Price"  // Set tab bar title.
+        priceTab.navigationBar.isTranslucent = false  // Set navigation translucent flag.
 
         percentageChangeTab.title = "Percentage Change"
         percentageChangeTab.navigationBar.isTranslucent = false
@@ -42,9 +46,11 @@ class TabBarViewController: UITabBarController {
         changeTab.title = "Change"
         changeTab.navigationBar.isTranslucent = false
 
+        // Add navigations to tab bar.
         setViewControllers([priceTab, percentageChangeTab, volumeTab, changeTab], animated: false)
-        tabBar.isTranslucent = false
+        tabBar.isTranslucent = false  // Set tab bar translucent flag.
         
+        // Add tab bar icons.
         guard let items = tabBar.items else { return }
         let icons = ["dollarsign.square", "chart.line.uptrend.xyaxis", "v.square", "chart.xyaxis.line"]
         for i in 0..<items.count {
