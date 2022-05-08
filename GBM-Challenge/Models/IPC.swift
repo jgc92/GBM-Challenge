d0ac07e4beccc700c1279523bee6576e8deb4f19
jgc92 <2539918+jgc92@users.noreply.github.com>
2022-05-08 18:22:10 -0500

    Add IPC model

---
+++ GBM-Challenge/Models/IPC.swift
@@ -0,0 +1,14 @@
+//
+//  IPC.swift
+//  GBM-Challenge
+//
+//  Created by Joaquín González Cervantes on 08/05/22.
+//
+
+struct IPC: Codable {
+    let date: String
+    let price: Double
+    let percentageChange: Double
+    let volume: Double
+    let change: Double
+}
