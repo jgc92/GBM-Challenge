5e35562112c2567694f296f80e8db2e8da35e99a
jgc92 <2539918+jgc92@users.noreply.github.com>
2022-05-08 18:03:37 -0500

    Initial commit

---
+++ GBM-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBM-Challenge
+//
+//  Created by Joaquín González Cervantes on 08/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
