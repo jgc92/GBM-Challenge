d654ed74107b12f60733e63b72f0bb32668334c5
jgc92 <2539918+jgc92@users.noreply.github.com>
2022-05-08 21:20:58 -0500

    Added date formatter to ViewModelHelper

--- GBM-Challenge/ViewModelHelper.swift
+++ GBM-Challenge/ViewModelHelper.swift
@@ -37,4 +37,27 @@ class ViewModelHelper {
             return nil
         }
     }
+    
+    // Format iso date string to human friendly readable date
+    func getFormattedDate(isoDate: String) -> String {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        if let date = dateFormatter.date(from: isoDate) {
+            let calendarDate = Calendar.current.dateComponents([.day, .year, .month, .hour, .minute, .second], from: date)
+            guard let day = calendarDate.day,
+                  let year = calendarDate.year,
+                  let month = calendarDate.month,
+                  let hour = calendarDate.hour,
+                  let minute = calendarDate.minute,
+                  let second = calendarDate.second
+            else {
+                return ""
+            }
+            return "Date: \(months[month-1])-\(day)-\(year)   Time: \(hour):\(minute):\(second)"
+        }
+        return ""
+    }
+
 }
